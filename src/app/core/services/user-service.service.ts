e7397ea4ab509b06eb6f111126eb7606282e1876
Juanrcaba <78881305+Juanrcaba@users.noreply.github.com>
2022-08-21 14:02:57 -0400

    Revert "Proposal/refactor service and guard"

--- src/app/core/services/user-service.service.ts
+++ src/app/core/services/user-service.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+interface userDataI{  
+    id:number,
+    fullName:string,
+    idRole : number,
+    token :string
+}
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class UserServiceService {
-  private readonly userData = new BehaviorSubject<string>('');
-  userData$ = this.userData.asObservable();
-  constructor() {}
 
-  set setData(data: string) {
-    this.userData.next(data);
+  userData$ = new  BehaviorSubject<string>("")
+  constructor() {
+  
+   }
+
+  get getData(){
+    return this.userData$.asObservable()
+  }
+
+  set setData(data:string){
+    this.userData$.next(data)
   }
 }
