4bb4669326a39506bb2d904ff1e45ba013dd879d
Nicolas Juanico <nicolas.juany@gmail.com>
2022-08-21 18:57:56 +0200

    Handle redirect to login when no data available

--- src/app/core/guard/auth-guard.guard.ts
+++ src/app/core/guard/auth-guard.guard.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { from, iif, map, Observable, of, switchMap } from 'rxjs';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { catchError, from, iif, map, Observable, of, switchMap } from 'rxjs';
 import { AuthserviceService } from 'src/app/module/authentication/services/authservice.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardGuard implements CanActivate {
-  constructor(private readonly authDb: AuthserviceService) {}
+  constructor(
+    private readonly authDb: AuthserviceService,
+    private readonly router: Router
+  ) {}
 
-  canActivate(): Observable<boolean> {
+  canActivate(): Observable<boolean | UrlTree> {
     const userData = JSON.parse(localStorage.getItem('userData') || 'false');
 
     return from(userData).pipe(
       switchMap((data) =>
         iif(
           () => !!data,
-          this.authDb.authToken(userData.token).pipe(map(() => true)),
-          of(true)
+          of(true),
+          this.authDb.authToken(userData.token).pipe(
+            catchError(() => of(this.router.createUrlTree(['/login']))),
+            map(() => this.router.createUrlTree(['/login']))
+          )
         )
       )
     );
